6e308372d41943e61c406539f51a2fd0548a1639
ahmadparvej <ahmadparvej58@gmail.com>
2024-04-21 19:32:58 +0530

    error resolve

--- src/components/custom/admin/revenueCard.tsx
+++ src/components/custom/admin/revenueCard.tsx
@@ -16,17 +16,17 @@ const RevenueCard = () => {
                 <Card x-chunk="dashboard-01-chunk-0">
                     <CardHeader className="pb-2">
                         <CardTitle className="text-sm font-medium flex justify-between">
-                            <span>Today's Revenue</span>
+                            <span>Today Revenue</span>
                             <span className="flex items-center">₹ 1100.00 <FaLongArrowAltUp className="text-green-500"/></span>
                         </CardTitle>
                     </CardHeader>
                     <CardContent className="p-6 pt-0">
                             <div className="text-sm font-medium flex justify-between pb-2">
-                                <span>Today's New Bookings</span>
+                                <span>Today New Bookings</span>
                                 <span className="flex items-center">10 <FaLongArrowAltUp className="text-green-500"/></span>
                             </div>
                             <div className="text-sm font-medium flex justify-between pb-2">
-                                <span>Today's Completed Service</span>
+                                <span>Today Completed Service</span>
                                 <span className="flex items-center">8 <FaLongArrowAltUp className="text-green-500"/></span>
                             </div>
                     </CardContent>
