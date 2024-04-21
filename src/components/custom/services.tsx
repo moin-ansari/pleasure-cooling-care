1177e4ab12c33fc13350909c5f2da8301ae1a3c2
ahmadparvej <ahmadparvej58@gmail.com>
2024-04-21 11:58:30 +0530

    added services cards

--- src/components/custom/services.tsx
+++ src/components/custom/services.tsx
@@ -1,23 +1,22 @@
 import Image from "next/image";
 import React from "react";
+import ServiceCard from "./serviceCard";
 
 const Services = ({ services }: any) => {
   return (
-    <div className="py-6 px-3 bg-primary flex justify-between">
-      <div>
-        <p className="text-lg font-semibold mb-1 text-primary-foreground">
+    <div className="py-6 px-3 bg-primary flex justify-between w-full">
+      <div className="w-full">
+      <h2 className="text-3xl font-bold text-white text-center mb-8">
           Services
-        </p>
-        <ul className="list-disc list-inside">
+        </h2>
+        {/* <ul className="list-disc list-inside">
           {services.map((service: any, index: any) => (
             <li key={index} className="text-primary-foreground">
               {service}
             </li>
           ))}
-        </ul>
-      </div>
-      <div className="max-w-sm rounded overflow-hidden shadow-lg flex">
-        <Image width={136} height={100} src="/ac_repair.jpg" alt="prototype" />
+        </ul> */}
+        <ServiceCard/>
       </div>
     </div>
   );
